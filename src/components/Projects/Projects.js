97c167216d5342242c7319e8d74adf83c7487c44
OliJah <jessy.coilliot@icloud.com>
2022-08-09 20:26:14 +0200

    add cv

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,10 +25,10 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Booki"
+              description="Low Cost AirBnB Template."
+              ghLink="https://github.com/OliJah/Booki-DW-P2"
+              demoLink="https://olijah.github.io/"
             />
           </Col>
 
